d99f9679c0505bfa6252cc0acb1597cb273a0b35
ArturoAR <43114558+hackAtmProject@users.noreply.github.com>
2021-03-20 23:19:08 -0600

    Add files via upload
    Adding Depth First Search Algorithm

---
+++ DFSNBishops.py
@@ -0,0 +1,21 @@
+
+def DFSNBishops(n):
+      InitialBoard = [ [ '-' for i in range(n) ] for j in range(n) ]
+      BoardStack = []
+      Boards = []
+      BoardStack.append(InitialBoard)
+      while(len(BoardStack) != 0):
+         CurrentBoard = BoardStack.pop()
+         Children = getChildrenBoards(CurrentBoard)
+         if(len(Children) == 0): 
+            if(len(Boards) == 0):
+                Boards.append(CurrentBoard)
+            else:
+                if(getNomberOfBishops(CurrentBoard) > getNomberOfBishops(Boards[-1]))
+                    Boards.pop()
+                    Boards.append(CurrentBoard)
+                elif (getNomberOfBishops(CurrentBoard) == getNomberOfBishops(Boards[-1]))
+                    Boards.append(CurrentBoard)
+	 else:
+            BoardStack.append(Children)
+
